0343bfea4283ea24697c45668fe4546006d3b3da
tfghykl <alexandralex0204@gmail.com>
2025-09-14 16:50:02 +0300

    Update (1) площадь и периметр квадрата.cpp

--- (1) площадь и периметр квадрата.cpp
+++ (1) площадь и периметр квадрата.cpp
@@ -2,11 +2,12 @@
 
 int main() {
 	int number{};
-	std::cout << "input  3 digit number";
+	std::cout << "input  3 digit number:\n";
 	while (number < 100 || number > 999) {
 		std::cin >> number;
 		if (number < 100 || number > 999) std::cout << "invalid value";
 	}
 	std::cout << number / 100 + 10 * (number % 100);
 	return 0;
+
 }
