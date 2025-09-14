2ff505a72fb483be087cb4a0bd245d407b7d6eb6
tfghykl <alexandralex0204@gmail.com>
2025-09-14 13:23:16 +0300

    Add files via upload

---
+++ (7) радиус и диаметр.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+	int circle_area{};
+	const double PI = 3.1415926;
+
+	std::cout << "input  circle area:\n";
+	while (circle_area < 1) {
+		std::cin >> circle_area;
+		if (circle_area < 1) std::cout << "invalid value\n";
+	}
+	std::cout << "radius is:\n" << sqrt (circle_area / PI) << '\n';
+
+	std::cout << "diameter is:\n" << 2 * sqrt(circle_area / PI);
+	return 0;
+}
