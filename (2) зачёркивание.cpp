025a11627efacc13a3e0b3f3ba8d34c29b736e64
tfghykl <alexandralex0204@gmail.com>
2025-09-14 13:04:20 +0300

    Add files via upload

---
+++ (2) зачёркивание.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+	int number{};
+	std::cout << "input  3 digit number:\n";
+	while (number < 100 || number > 999) {
+		std::cin >> number;
+		if (number < 100 || number > 999) std::cout << "invalid value\n";
+	}
+	std::cout << "changed number is:\n" << number / 100 + 10 * (number % 100);
+	return 0;
+}
---
+++ (5) перевёрнутое трёхзначное.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+	int number{};
+	std::cout << "input  3 digit number:\n";
+	while (number < 100 || number > 999) {
+		std::cin >> number;
+		if (number < 100 || number > 999) std::cout << "invalid value\n";
+	}
+	std::cout << "you number backwards is:\n" << (number / 100) + (number % 10) * 100 + 10 * (number / 10 % 10) << '\n';
+	return 0;
+}
