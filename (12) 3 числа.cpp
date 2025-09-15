49f4c01529bf7db6eb0d1e34ca41f6e2ba148b72
tfghykl <alexandralex0204@gmail.com>
2025-09-15 06:56:42 +0300

    Update (12) 3 числа.cpp

--- (12) 3 числа.cpp
+++ (12) 3 числа.cpp
@@ -16,6 +16,7 @@ int main() {
 	if ((a == biggest && b == smallest) || (a == smallest && b == biggest)) middle = c;
 	if ((c == biggest && b == smallest) || (c == smallest && b == biggest)) middle = a;
 	if ((a == biggest && c == smallest) || (a == smallest && c == biggest)) middle = b;
-	std::cout << smallest << ' ' << middle << ' ' << biggest;
+	std::cout << "smallest is:\n"<< smallest << "\n biggest is:\n" << biggest << "\n average is:\n << (a+b+c)/3.0 << "\n mult is\n" << a * b * c;
 	return 0;
+
 }
